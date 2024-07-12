e973a7db75de926e37e34c64c1981113e82415b7
Matthew Watson <Me@MatthewTheDeveloper.com>
2024-07-12 11:09:21 -0400

    - init project - added Angular Material - removed routing - added form demo - added populated field label slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+  ],
+  providers: [
+    { 
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        subscriptSizing: 'dynamic',
+      }
+    }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'larger-form-labels';
+  labelSize = signal<number>(16);
+
+  constructor() {
+    effect(() => {
+      const size = this.labelSize();
+
+      document.documentElement.style.setProperty('--mat-form-field-outlined-label-text-populated-size', `${size}px`);
+    });
+  }
+
+  formatLabel(value: number): string {
+    return `${value}px`;
+  }
 }
